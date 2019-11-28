7a80509e96df714c2d4d37f0cd31a9d3ec5504f6
jonatas junior <jonatas.junior@acad.ifma.edu.br>
2019-11-28 11:33:23 -0300

    cadastro despesa

---
+++ cmake-build-debug/CMakeFiles/acai_h.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/acai_h.dir/main.c.o"
+  "acai_h.exe"
+  "acai_h.pdb"
+  "libacai_h.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/acai_h.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "despesas.h"
+
+int main() {
+    cadastra_despesa();
+    return 0;
+}
