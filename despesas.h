57f33a6b48651b2790fb294da3dec0112dec7c6d
Sherlyson <sherlyson.silvagoncalves@gmail.com>
2019-12-05 17:29:39 -0300

    Adição do Estoque e das funções de cadastrar e visualizar estoque

--- despesas.h
+++ despesas.h
@@ -9,43 +9,43 @@ typedef struct{
     float valor_despesa;
 }despesa;
 
-   void cadastra_despesa(){
-       FILE *arquivo_despesa;
-       despesa *custo;
-       int i = 0;
-       int k = 0;
-       int opcao = 1;
-       custo = (despesa*)malloc((i+1)*sizeof(despesa));
-       arquivo_despesa = fopen("despesas.txt","rb");
+void cadastra_despesa(){
+	FILE *arquivo_despesa;
+	despesa *custo;
+	int i = 0;
+	int k = 0;
+    int opcao = 1;
+	custo = (despesa*)malloc((i+1)*sizeof(despesa));
+	arquivo_despesa = fopen("despesas.txt","rb");
 
-            while (fread(&custo[k], sizeof(custo[k]), 1,arquivo_despesa)){
-                custo =(despesa*)realloc(custo,(k+2)*sizeof(despesa));
-                k++;
-            }
+    while (fread(&custo[k], sizeof(custo[k]), 1,arquivo_despesa)){
+		custo =(despesa*)realloc(custo,(k+2)*sizeof(despesa));
+        k++;
+    }
 
 
-       while (opcao != 0){
-           custo = (despesa*)realloc(custo,(i+1)*sizeof(despesa));
+	while (opcao != 0){
+    custo = (despesa*)realloc(custo,(i+1)*sizeof(despesa));
 
-           printf("Digite o nome da despesa\n");
-           scanf("%s",custo[i].nome_despesa);
+    printf("Digite o nome da despesa\n");
+    scanf("%s",custo[i].nome_despesa);
 
-           printf("Digite o valor da despesa\n");
-           scanf("%f", &custo[i].valor_despesa);
+    printf("Digite o valor da despesa\n");
+    scanf("%f", &custo[i].valor_despesa);
 
-           printf("Digite 1 para continuar o cadastramento\n");
-           printf("Digite 0 para voltar ao menu principal\n");
-           scanf("%d",&opcao);
+    printf("Digite 1 para continuar o cadastramento\n");
+    printf("Digite 0 para voltar ao menu principal\n");
+    scanf("%d",&opcao);
 
-           i++;
-       }
+	i++;
+	}
 
-       if (opcao == 0){
-           arquivo_despesa = fopen("despesas.txt","wb");
-           for (int j = 0; j < i ; j++) {
-               printf("%s\n", custo[j].nome_despesa);
-               printf("%.2f\n", custo[j].valor_despesa);
-               fwrite(&custo[j], sizeof(custo[j]), 1,arquivo_despesa);
-           }
-       }
-   }
+	if (opcao == 0){
+		arquivo_despesa = fopen("despesas.txt","wb");
+        for (int j = 0; j < i ; j++) {
+			printf("%s\n", custo[j].nome_despesa);
+			printf("%.2f\n", custo[j].valor_despesa);
+			fwrite(&custo[j], sizeof(custo[j]), 1,arquivo_despesa);
+		}
+	}
+}
--- menu.h
+++ menu.h
@@ -4,14 +4,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "despesas.h"
+#include "estoque.h"
 
  void menu() {
     int opcao;
-    printf("Digite 1 para cadastrar novas despesas\n");
+    printf("****************MENU PRINCIPAL****************"zn);
+    printf("Selecione uma das opções a baixo:\n");
+    printf("1 - cadastrar novas despesas\n");
+    printf("2 - cadastrar novos produtos no estoque\n");
     scanf("%d",&opcao);
-     switch(opcao){
-         case 1:
-             cadastra_despesa();
-             break;
-     }
+    switch(opcao){
+		case 1:
+			cadastra_despesa();
+			break;
+		case 2
+			cadastra_produto();
+			break;
+		case 0;
+			return 0;
+			break;
+			
+    }
 };
