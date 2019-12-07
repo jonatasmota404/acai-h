34db173b7126b95b07014008e6e5ef00038c262f
jonatas junior <jonatas.junior@acad.ifma.edu.br>
2019-12-07 09:01:35 -0300

    alteracao da branch

--- despesas.h
+++ despesas.h
@@ -14,7 +14,6 @@ void cadastra_despesa(){
     FILE *arquivo_despesa;
     despesa *custo;
     int i = 0;
-    int id = 0;
     int opcao = 1;
     custo = (despesa*)malloc((i+2)*sizeof(despesa));
     arquivo_despesa = fopen("despesas.txt","ab");
@@ -38,7 +37,6 @@ void cadastra_despesa(){
             for (int j = 0; j < i; ++j) {
                 printf("%s\n", custo[j].nome_despesa);
                 printf("%f\n", custo[j].valor_despesa);
-                id++;
             }
         }
     }
@@ -88,65 +86,25 @@ void pesquisa_nome(){
     }
 }
 
-void deleta_item(){
+void deleta_item() {
     FILE *arquivo_despesa;
     despesa *custo;
     char nome[50];
     int i = 0;
-    arquivo_despesa = fopen("despesas.txt","wb");
-    custo = (despesa*)malloc(sizeof(despesa));
-    if(arquivo_despesa == NULL){
+    arquivo_despesa = fopen("despesas.txt", "wb");
+    custo = (despesa *) malloc(sizeof(despesa));
+    if (arquivo_despesa == NULL) {
         printf("erro na abertura do arquivo\n");
     } else {
         printf("Digite o nome da despesa que deseja\n");
-        scanf("%s",nome);
-        while (fread(&custo[i], sizeof(despesa),1,arquivo_despesa) == 1){
-            if (strcmp(nome, custo[i].nome_despesa) == 0){
-               //custo[i].nome_despesa = '\0';
-               //custo[i].valor_despesa = 0;
+        scanf("%s", nome);
+        while (fread(&custo[i], sizeof(despesa), 1, arquivo_despesa) == 1) {
+            if (strcmp(nome, custo[i].nome_despesa) == 0) {
+                //custo[i].nome_despesa = '\0';
+                //custo[i].valor_despesa = 0;
             }
             i++;
         }
         fclose(arquivo_despesa);
     }
-
-void cadastra_despesa(){
-	FILE *arquivo_despesa;
-	despesa *custo;
-	int i = 0;
-	int k = 0;
-    int opcao = 1;
-	custo = (despesa*)malloc((i+1)*sizeof(despesa));
-	arquivo_despesa = fopen("despesas.txt","rb");
-
-    while (fread(&custo[k], sizeof(custo[k]), 1,arquivo_despesa)){
-		custo =(despesa*)realloc(custo,(k+2)*sizeof(despesa));
-        k++;
-    }
-
-
-	while (opcao != 0){
-    custo = (despesa*)realloc(custo,(i+1)*sizeof(despesa));
-
-    printf("Digite o nome da despesa\n");
-    scanf("%s",custo[i].nome_despesa);
-
-    printf("Digite o valor da despesa\n");
-    scanf("%f", &custo[i].valor_despesa);
-
-    printf("Digite 1 para continuar o cadastramento\n");
-    printf("Digite 0 para voltar ao menu principal\n");
-    scanf("%d",&opcao);
-
-	i++;
-	}
-
-	if (opcao == 0){
-		arquivo_despesa = fopen("despesas.txt","wb");
-        for (int j = 0; j < i ; j++) {
-			printf("%s\n", custo[j].nome_despesa);
-			printf("%.2f\n", custo[j].valor_despesa);
-			fwrite(&custo[j], sizeof(custo[j]), 1,arquivo_despesa);
-		}
-	}
 }
--- menu.h
+++ menu.h
@@ -11,37 +11,22 @@
     printf("Digite 1 para cadastrar novas despesas\n");
     printf("Digite 2 para ler o arquivo das despesas\n");
     printf("Digite 3 pesquisar por nome\n");
-    scanf("%d",&opcao);
+     printf("****************MENU PRINCIPAL****************\n");
+     printf("Selecione uma das opções a baixo:\n");
+     printf("1 - cadastrar novas despesas\n");
+     printf("2 - cadastrar novos produtos no estoque\n");
+     scanf("%d",&opcao);
      switch(opcao){
          case 1:
              cadastra_despesa();
              break;
          case 2:
-             ler_arquivo();
-             break;
-         case 3:
-             pesquisa_nome();
+             cadastra_produto();
              break;
          default:
-             printf("operação inválida");
+             printf("opcao invalida");
              break;
      }
 };
-    printf("****************MENU PRINCIPAL****************\n");
-    printf("Selecione uma das opções a baixo:\n");
-    printf("1 - cadastrar novas despesas\n");
-    printf("2 - cadastrar novos produtos no estoque\n");
-    scanf("%d",&opcao);
-    switch(opcao){
-		case 1:
-			cadastra_despesa();
-			break;
-		case 2
-			cadastra_produto();
-			break;
-		case 0;
-			return 0;
-			break;
-			
-    }
-};
+
+
