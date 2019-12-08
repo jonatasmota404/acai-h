05f0ff675ce5e7c645b15c082307077b9f2b3781
jonatas junior <jonatas.junior@acad.ifma.edu.br>
2019-12-08 16:16:22 -0300

    laço de repeticao no menu

--- menu.h
+++ menu.h
@@ -27,22 +27,53 @@ void menu_despesa(){
     }
 }
 
+void menu_estoque(){
+    int opcao;
+    printf("****************MENU ESTOQUE****************\n");
+    printf("Selecione uma das opções a baixo:\n");
+    printf("1 - cadastrar novos produtos no estoque\n");
+    scanf("%d",&opcao);
+    switch(opcao){
+        case 1:
+            cadastra_produto();
+            break;
+        case 2:
+            //cadastra_produto();
+            break;
+        default:
+            printf("opcao invalida");
+            break;
+    }
+}
+
+void menu_compra_venda(){
+
+}
+
  void menu() {
     int opcao;
-     printf("****************MENU PRINCIPAL****************\n");
-     printf("Selecione uma das opções a baixo:\n");
-     printf("1 - para ir ao menu das despesas\n");
-     printf("2 - cadastrar novos produtos no estoque\n");
-     scanf("%d",&opcao);
-     switch(opcao){
-         case 1:
-             menu_despesa();
-             break;
-         case 2:
-             cadastra_produto();
-             break;
-         default:
-             printf("opcao invalida");
-             break;
-     }
+    int true = 1;
+    while (true != 0) {
+        printf("****************MENU PRINCIPAL****************\n");
+        printf("Selecione uma das opções a baixo:\n");
+        printf("0 - para sair\n");
+        printf("1 - para ir ao menu das despesas\n");
+        printf("2 - cadastrar novos produtos no estoque\n");
+        scanf("%d", &opcao);
+        switch (opcao) {
+            case 0:
+                printf("Voce saiu\n");
+                true = 0;
+                break;
+            case 1:
+                menu_despesa();
+                break;
+            case 2:
+                menu_estoque();
+                break;
+            default:
+                printf("opcao invalida");
+                break;
+        }
+    }
 }
