fea1f35495e4304e3d61d78478bf82ff5ddada1d
jonatas junior <jonatas.junior@acad.ifma.edu.br>
2019-12-11 01:50:01 -0300

    bug estatística arrumado

--- estatistica.h
+++ estatistica.h
@@ -5,46 +5,47 @@
 
 typedef struct{
 	int conta;
-	data_estoque data_estistica;
+	data_estoque data_estatistica;
 }count;
 
 void mais_pedidos(){
 	produto *prod;
 	acai *acai_p;
 	count *contador;
+    int quantidade_de_pedidos = 0;
 	
 	FILE *arquivo_estoque;
 	FILE *arquivo_venda;
 	FILE *arquivo_estatistica;
 	int k=0;
-	int l=0;
-	int j=0;
-	int count;
+	int i=0;
+	int l =0;
+
 	
-	contador = (count*)malloc((1)*sizeof(count));
+	contador = (count *)malloc((1)*sizeof(count));
 	prod = (produto*)malloc((1)*sizeof(produto));
 	acai_p = (acai*)malloc((1)*sizeof(acai));
 	
 	arquivo_estoque = fopen("estoque.txt", "rb");
 	arquivo_venda = fopen("venda.txt", "rb");
 	arquivo_estatistica = fopen("estatistica.txt", "ab");
-	
-	printf("Digite o data de hoje\n");
-    scanf("%d %d %d", &add[quantidade_de_prod].dataEstoque_compra.dia,
-		&add[quantidade_de_prod].dataEstoque_compra.mes,
-        &add[quantidade_de_prod].dataEstoque_compra.ano);
+
+    printf("Digite o data de hoje\n");
+    scanf("%d %d %d", &contador[quantidade_de_pedidos].data_estatistica.dia,
+          &contador[quantidade_de_pedidos].data_estatistica.mes,
+          &contador[quantidade_de_pedidos].data_estatistica.ano);
 	
 	if(arquivo_estoque == NULL){
 		printf("ERRO!");
 	}else{
-		while(fread(prod[l], sizeof(prod[l]), 1, arquivo_estoque) == 1){
+		while(fread(&prod[l], sizeof(prod[l]), 1, arquivo_estoque) == 1){
 			prod = (produto*) realloc(prod,(l+2) * sizeof(produto));
 		}
 		fclose(arquivo_estoque);
 		if(arquivo_venda == NULL){
 		printf("Nenhuma venda realizada");
 		}else{
-			while(fread(acai_p[k], sizeof(acai_p[k]), 1, arquivo_venda) == 1){
+			while(fread(&acai_p[k], sizeof(acai_p[k]), 1, arquivo_venda) == 1){
 				acai_p = (acai*) realloc(acai_p,(k+2) *  sizeof(acai));
 				k++;
 			}
@@ -73,5 +74,6 @@ void mais_pedidos(){
 				printf("ID: %d | Produto: %s | Quantidade de vezes pedido: %d", i, prod[i].nome_produto, contador[i].conta);
 			}
 		}
+		quantidade_de_pedidos++;
 	}
 }
